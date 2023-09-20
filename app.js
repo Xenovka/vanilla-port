2e3fdce933c3b2c85a986b09de1bd095ca959d4e
Wendy <wendyanto.bukitvista@gmail.com>
2023-09-20 21:35:20 +0700

    fixing image aspect on zoom effect

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ export default class Sketch {
         time: { value: 1.0 },
         uProgress: { value: 1.0 },
         uTexture: { value: new THREE.TextureLoader().load(testTexture) },
+        uTextureSize: { value: new THREE.Vector2(100, 100) },
         uResolution: { value: new THREE.Vector2(this.width, this.height) },
         uQuadSize: { value: new THREE.Vector2(300, 300) }
       },
@@ -73,6 +74,7 @@ export default class Sketch {
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     this.scene.add(this.mesh);
     this.mesh.position.x = 300;
+    this.mesh.rotation.z = 0.5;
   }
 
   render() {
